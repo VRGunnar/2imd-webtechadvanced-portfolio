9c746ee3f48e5e60823092c2ff31aa5737b0b40c
Gunnar Van Remoortere <gunnarvanremoortere@hotmail.com>
2021-03-16 17:56:35 +0100

    video aan het volgen, geolocation werkt so far

---
+++ lab4- API Prototype/app.js
@@ -0,0 +1,19 @@
+class App {
+    constructor() {
+        this.getLocation();
+    }
+
+    getLocation(){
+        navigator.geolocation.getCurrentPosition(this.gotLocation, this.errorLocation);
+    }
+
+    gotLocation(result){
+        console.log(result);
+    }
+
+    errorLocation(error) {
+        console.log(error);
+    }
+}
+
+let app = new App();
