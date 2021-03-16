c09443ecbcb589c5daabcf7977aad54e2b21a6b0
Gunnar Van Remoortere <gunnarvanremoortere@hotmail.com>
2021-03-16 19:21:29 +0100

    alles van video geschreven, data ophalen werkt

--- lab4- API Prototype/app.js
+++ lab4- API Prototype/app.js
@@ -1,14 +1,34 @@
 class App {
     constructor() {
         this.getLocation();
+        this.latitude;
+        this.longitude;
     }
 
     getLocation(){
-        navigator.geolocation.getCurrentPosition(this.gotLocation, this.errorLocation);
+        navigator.geolocation.getCurrentPosition(this.gotLocation.bind(this), this.errorLocation.bind(this));
     }
 
     gotLocation(result){
-        console.log(result);
+        this.latitude = result.coords.latitude;
+        this.longitude = result.coords.longitude;
+        this.getWeather();
+    }
+
+    getWeather() {
+        //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+        const apiKey = "333d9555968f7bddd1b2e207ac0da7d1";
+        let url =`https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?lat=${this.latitude}&lon=${this.longitude}&appid=${apiKey}&units=metric`;
+        fetch(url).then(response => {
+            console.log(response);
+            return response.json();
+        }).then(data => {
+            console.log(data);
+            document.querySelector("#weather").innerHTML = data.main.temp;
+
+        }).catch(error => {
+            console.log(error);
+        });
     }
 
     errorLocation(error) {
